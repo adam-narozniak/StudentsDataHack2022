2252234b22c6a952d012c1deb12ae6b1a614a641
Adam Narozniak <narozniak.adam4@gmail.com>
2022-05-08 08:58:09 +0200

    add preprocessing

---
+++ sdh/preprocessing/numerical_tranformers.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fba097f294a14eabe4333d6339a9f2a0b6715f245e7ab204b0f51f7163d3714c
+size 459
---
+++ sdh/preprocessing/ordinal_transformers.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8bbc7950d86ce75850d7082c7f409387e23409bb1260d675be84b022b1587486
+size 2275
